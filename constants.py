fca491f1cddc2befd94d5efef216728315c45698
rishabh negi <ronynegi676@gmail.com>
2024-07-08 11:57:21 +0530

    Analog watch using opencv

---
+++ constants.py
@@ -0,0 +1,10 @@
+#Defining colours for clock makeup
+#here we are making a dictionary of colours so that we may access them wherever needed
+COLORS ={'blue': (255,0,0), 'green': (0,255,0),'red': (0,0,255),'yellow':(0,255,255),'magneta':(255,0,255)
+         ,'cyan':(255,255,0),'white':(255,255,255), 'amber':(255,191,0),'gray':(125,125,125),'dark_gray':
+             (50,50,50),'light_gray':(220,220,220),'black': (0,0,0)}
+
+#radius of the clock
+RADIUS =260
+CENTER =(320,320)
+CANVAS_SIZE =(640,640,3)
